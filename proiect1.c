69906aba42c301ffa1ef4f78cd3cacc03819f16e
AlexandraGruia <149401089+AlexandraGruia@users.noreply.github.com>
2023-10-30 17:47:41 +0200

    Update proiect1.c

--- proiect1.c
+++ proiect1.c
@@ -2,6 +2,19 @@
 #include <sys/stat.h>
 
 
+void user(struct stat st)
+{
+	if(st.st_mode & S_IRWXU)
+		printf("drepturi de acces user: RWX\n");
+	else if(st.st_mode & S_IRUSR)
+		printf("drepturi de acces user: R--\n");
+	else if(st.st_mode & S_IWUSR)
+		printf("drepturi de acces user: -W-\n");
+	else if(st.st_mode & S_IXUSR)
+		printf("drepturi de acces user: --X\n");
+	else printf("---\n");
+}
+
 int main(int argc,char *argv[])
 {
 	struct stat st;
@@ -16,7 +29,10 @@ int main(int argc,char *argv[])
 	if(S_ISREG(st.st_mode))
 	{
 		printf("Este fisier!\n");
+
 	}else printf("EROARE\n");
+	
 		
+	
 	return 0;
 }
