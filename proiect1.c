74d6dd8da70726b91e569033504109b4f5fa7650
AlexandraGruia <149401089+AlexandraGruia@users.noreply.github.com>
2023-11-04 01:18:59 +0200

    Update proiect1.c

--- proiect1.c
+++ proiect1.c
@@ -1,38 +1,58 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-
 void user(struct stat st)
 {
-	if(st.st_mode & S_IRWXU)
-		printf("drepturi de acces user: RWX\n");
-	else if(st.st_mode & S_IRUSR)
-		printf("drepturi de acces user: R--\n");
-	else if(st.st_mode & S_IWUSR)
-		printf("drepturi de acces user: -W-\n");
-	else if(st.st_mode & S_IXUSR)
-		printf("drepturi de acces user: --X\n");
-	else printf("---\n");
+	printf("Drepturile de acces pentru useri: ");
+	if(st.st_mode & S_IRUSR) printf("R");
+   	else printf("-");
+	if(st.st_mode & S_IWUSR) printf("W");
+   	else printf("-");
+	if(st.st_mode & S_IXUSR) printf("X");
+    	else printf("-");
+   	printf("\n");
+}
+
+void grup(struct stat st)
+{
+ 	printf("Drepturile de acces pentru grup: ");
+   	if(st.st_mode & S_IRGRP) printf("R");
+   	else printf("-");
+	if(st.st_mode & S_IWGRP) printf("W");
+   	else printf("-");
+	if(st.st_mode & S_IXGRP) printf("X");
+   	else printf("-");
+   	printf("\n");
+}
+
+void altii(struct stat st)
+{
+	printf("Drepturile de acces pentru altii: ");
+	if (st.st_mode & S_IROTH) printf("R");
+	else printf("-");
+	if (st.st_mode & S_IWOTH) printf("W");
+	else printf("-");
+	if (st.st_mode & S_IXOTH) printf("X");
+	else printf("-");
+	printf("\n");
 }
 
 int main(int argc,char *argv[])
 {
 	struct stat st;
-	
 	if(argc==2)
 	{
-		printf("Argumentele sunt: %s %s\n",argv[0],argv[1]);
-		
+		printf("Argumentele sunt: %s %s\n",argv[0],argv[1]);	
 	}else printf("EROARE\n");
-	
+
 	stat(argv[1],&st);
 	if(S_ISREG(st.st_mode))
 	{
 		printf("Este fisier!\n");
-
+       		user(st);
+		grup(st);
+		altii(st);
 	}else printf("EROARE\n");
-	
-		
 	
 	return 0;
 }
